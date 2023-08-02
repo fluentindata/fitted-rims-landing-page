99f0ab989ed3ee1155d96d5b2d9b7d3c3d08f48b
WaliSiddiqui <siddiqui4430@hotmail.com>
2023-08-02 09:34:42 -0700

    init commit

---
+++ src/app/page.js
@@ -0,0 +1,80 @@
+ import "./page.css"
+
+const page = () => {
+  return (
+    <>
+    <title> Boost Tire Sales Online and In-store</title>
+    <div className="container">
+    <div className="wrapper">
+    <div className="dropdown">
+  <h5 className="drop">Client Support</h5>
+  <div className="dropdown-content">
+    <a href="#">92384834</a>
+    <a href="#">Support request</a>
+    <a href="#">Knowledge </a>
+  </div>
+</div>
+<h5 className="login">Login</h5>
+<h5 className="register">Register</h5>
+</div>
+
+
+<div className="header-text max-w884">
+  <h1 className="heading-2">Tire Sales and Procurement Solutions </h1>
+  <p className="paragraph-xl header">
+    Maximize your tire business from sales to sourcing.&nbsp;Set your business up to win with advanced sales and procurement tools powered by real-time inventory data, in-store and online.
+    </p>
+    </div>
+  
+<div className="cta-top new-home">
+  <div className="cta-top__l">
+    <h2 className="cta-top__heading">Our bottom line is to raise yours.</h2>
+    <p>Boost business, and customer satisfaction with a fully integrated, comprehensive solution that’s flexible to all your business’s tire-related needs.</p>
+    <div className="stats-wrapper">
+      <div className="stats-card">
+        <div className="stats-card__heading">
+        <h3 className="heading-s">6,500</h3></div>
+        <p className="stats-card__p">Locations across North<br></br>America use TireConnect</p></div>
+        <div className="stats-card">
+        <div className="stats-card__heading"><h3 className="heading-s">23M</h3></div>
+        <p className="stats-card__p">Customers used TireConnect<br></br>on our clients’ websites</p></div>
+        <div className="stats-card">
+          <div className="stats-card__heading">
+          <h3 className="heading-s">54M</h3></div>
+          <p className="stats-card__p">Tire searches delivered</p></div>
+        <div className="stats-card">
+          <div className="stats-card__heading"><h3 className="heading-s">13M</h3></div>
+         <p className="stats-card__p">Leads captured for our clients</p></div></div></div>
+        <div id="w-node-_59cedee5-e92d-d481-93b5-e6c357bf2e36-0776d7b9" className="cta-top__r bg-red"><h3 className="heading-xs">See how TireConnect can help you boost your revenue and customer satisfaction:</h3>
+      <div div className="cta-top__video-wrapper"><a href="#" className="cta-top__video w-inline-block w-lightbox" aria-label="open lightbox" aria-haspopup="dialog">
+  </a></div></div></div>
+  </div>
+
+
+    <div className="clients-segment">
+      <h1>Our Clients</h1>
+      
+    </div>
+
+    <div className="solutions-segment">
+      <h1>See Our Solutions</h1>
+    </div>
+
+    <div className="wrapper-flex margin-t100">
+      <div  className="flex-l">
+        <h2 className="heading-m ">By Audience </h2>
+        <p className="paragraph-1 ">Solutions made for you, your business, and your customers.</p></div>
+        <div  className="flex-r">
+          <img src="https://uploads-ssl.webflow.com/61f95ed9bf2aea22dfc2f8b6/61f95ed9bf2aeac509c2f95f_solutions-audience.svg" loading="lazy" alt="Chart"/>
+          </div></div>
+
+
+          
+          <div className="button-wrapper margin-t30"><button className="button-red w-button">See All Solutions</button></div>
+
+
+    </>
+  )
+}
+
+export default page
