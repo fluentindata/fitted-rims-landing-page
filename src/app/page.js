a0fa1adc6c7285b06f1fb35fa8543cf6498bdb29
WaliSiddiqui <siddiqui4430@hotmail.com>
2023-08-02 17:24:56 -0700

    "[02/08/2023] landing page till benefits"

--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ const page = () => {
   
 <div className="cta-top new-home">
   <div className="cta-top__l">
-    <h2 className="cta-top__heading">Our bottom line is to raise yours.</h2>
+    <h1 className="cta-top__heading">Our bottom line is to raise yours.</h1>
     <p>Boost business, and customer satisfaction with a fully integrated, comprehensive solution that’s flexible to all your business’s tire-related needs.</p>
     <div className="stats-wrapper">
       <div className="stats-card">
@@ -46,7 +46,7 @@ const page = () => {
           <div className="stats-card__heading"><h3 className="heading-s">13M</h3></div>
          <p className="stats-card__p">Leads captured for our clients</p></div></div></div>
         <div id="w-node-_59cedee5-e92d-d481-93b5-e6c357bf2e36-0776d7b9" className="cta-top__r bg-red"><h3 className="heading-xs">See how TireConnect can help you boost your revenue and customer satisfaction:</h3>
-      <div div className="cta-top__video-wrapper"><a href="#" className="cta-top__video w-inline-block w-lightbox" aria-label="open lightbox" aria-haspopup="dialog">
+      <div  className="cta-top__video-wrapper"><a href="#" className="cta-top__video w-inline-block w-lightbox" aria-label="open lightbox" aria-haspopup="dialog">
   </a></div></div></div>
   </div>
 
@@ -56,6 +56,20 @@ const page = () => {
       
     </div>
 
+    <div class="ticker">
+  <div class="ticker__list" >
+    <div class="ticker__item"><img src="https://img.clock.co.uk/250x100?text=Item 1&amp;color=f3e5f5"/></div>
+    <div class="ticker__item"><img src="https://img.clock.co.uk/300x100?text=Item 2&amp;color=ede7f6"/></div>
+    <div class="ticker__item"><img src="https://img.clock.co.uk/200x100?text=Item 3&amp;color=ffcdd2"/></div>
+    <div class="ticker__item"><img src="https://img.clock.co.uk/100x100?text=Item 4&amp;color=f8bbd0"/></div>
+    <div class="ticker__item"><img src="https://img.clock.co.uk/350x100?text=Item 5&amp;color=e8eaf6"/></div>
+    <div class="ticker__item"><img src="https://img.clock.co.uk/100x100?text=Item 6&amp;color=e3f2fd"/></div>
+    
+  </div>
+</div>
+
+  
+
     <div className="solutions-segment">
       <h1>See Our Solutions</h1>
     </div>
@@ -63,15 +77,208 @@ const page = () => {
     <div className="wrapper-flex margin-t100">
       <div  className="flex-l">
         <h2 className="heading-m ">By Audience </h2>
-        <p className="paragraph-1 ">Solutions made for you, your business, and your customers.</p></div>
+        <p className="paragraph-1 ">Solutions made for you, your business, and your<br/> customers.</p></div>
         <div  className="flex-r">
           <img src="https://uploads-ssl.webflow.com/61f95ed9bf2aea22dfc2f8b6/61f95ed9bf2aeac509c2f95f_solutions-audience.svg" loading="lazy" alt="Chart"/>
           </div></div>
-
+          <br></br><br></br><br></br>
 
           
-          <div className="button-wrapper margin-t30"><button className="button-red w-button">See All Solutions</button></div>
+    <div className="row">
+     <div className="column">
+       <h3 className="singleLocation">Single-Location Tire Dealers </h3>
+       <p>Qualified service appointment requests are secured  <br/> 24/7 with a simple scheduling process. </p>
+       <p className="learnMore">Learn More</p>
+    </div>
+     <div className="column">
+      <h3 className="multiLocation">Multi-Location Tire Dealers</h3>
+      <p> Tools to manage clients, configure pricing , display <br/> on hand inventory , set up deliver and more</p> 
+      <p className="learnMore">Learn More</p>
+      <br/> <br/><br/>
+     </div>
+     
 
+     <div className="column">
+       <h3 className="automotive">Automotive Shops </h3>
+       <p>Deliver faster quotes, make more sales, and optimize <br/>your in-store resources. </p>
+       <p className="learnMore">Learn More</p>
+    </div>
+     <div className="column">
+      <h3 className="b2bDealers">B2B Tire Dealers</h3>
+      <p> Configure, manage and scale multiple locations with <br/> flexibility or consistency across all stores.</p> 
+      <p className="learnMore">Learn More</p>
+      <br/> <br/><br/>
+
+     </div>
+
+
+     <div className="column">
+       <h3 className="dealership">Car Dealerships  </h3>
+       <p>Dealerships across North America are using  <br/> FittedTyres to increase speed and efficiency for in- <br/> store tire quoting  </p>
+       <p className="learnMore">Learn More</p>
+    </div>
+     <div className="column">
+      <h3 className="enterprise">Enterprise and Developers </h3>
+      <p> Developers are using the FittedTyres platform to <br/> customize tire sales solutions for their clients..</p> 
+      <p className="learnMore">Learn More</p>
+      <br/> <br/>
+     </div>
+    </div>
+          
+    <div className="button-wrapper margin-t30"><button className="button-red w-button">See All Solutions</button></div>
+    
+
+
+    <div className="wrapper-flex margin-t100">
+      <div className="flex-l"><img src="https://uploads-ssl.webflow.com/61f95ed9bf2aea22dfc2f8b6/61f95ed9bf2aea863dc2f97b_handshake.svg" loading="lazy" alt="Handshake" style={{marginLeft:"90px"}}/></div>
+      <div className="flex-r is-changed" style={{marginRight:"100px"}}>
+      <h1 className="heading-m text-red" style={{marginRight:"180px"}} >By Need</h1>
+      <p className="paragraph-l mw-412" >Solutions made for you, your business, and your customers.</p> </div>
+
+    </div>
+    <br/> <br/><br/> <br/><br/> <br/>
+
+    <div className="row">
+     <div className="column">
+       <h3 className="singleLocation">Sell Tires and Wheels Online </h3>
+       <p>Effortlessly capture qualified leads and enable  <br/> eCommerce on your website to sell more tires online </p>
+       <p className="learnMore">Learn More</p>
+    </div>
+     <div className="column">
+      <h3 className="multiLocation">Sell Tires In-Store</h3>
+      <p> Provide a collaborative, visual and engaging <br/> customer experience in your tire shop. </p>
+      <p className="learnMore">Learn More</p>
+      <br/> <br/><br/>
+     </div>
+     
+
+     <div className="column">
+       <h3 className="automotive">Inventory Connectivity & <br/>Procurement  </h3>
+       <p>Integrate your inventory into the FittedTyres system <br/> to take advantage of a network that reaches more <br/>tyre shop customers </p>
+       <p className="learnMore">Learn More</p>
+    </div>
+     <div className="column">
+      <h3 className="b2bDealers">Sell AutoMotive Services</h3>
+      <p> Go full eCommerce or use a simple catalog look-up <br/> to increase your automotive  service revenue</p> 
+      <p className="learnMore">Learn More</p>
+      <br/> <br/><br/>
+
+     </div>
+
+
+     <div className="column">
+       <h3 className="dealership">Wholesale Tires  </h3>
+       <p>Allow your clients to order tyres with just a few clicks   <br/>so that you can increase your B2B tire sales  </p>
+       <p>Learn More</p>
+    </div>
+     <div className="column">
+      <h3 className="enterprise">Custom Integrations </h3>
+      <p> Easily connect your custom integrations with your tire  <br/> sales software using FittedTyres </p> 
+      <p className="learnMore">Learn More</p>
+      <br/> <br/>
+     </div>
+    </div>
+          
+    <div className="button-wrapper margin-t30"><button className="button-red w-button">See All Solutions</button></div>
+    <br/>  <br/> 
+
+
+    <div className="wrapper-flex margin-t100">
+      <div  className="flex-l">
+        <h2 className="heading-g ">Our Products </h2>
+        <p className="paragraph-1 ">See our suite of sales and procurement products designed<br/> to meet the needs of your business</p></div>
+        <br/>
+
+        <div  className="flex-r">
+          <img src="https://uploads-ssl.webflow.com/61f95ed9bf2aea22dfc2f8b6/61f95ed9bf2aeac509c2f95f_solutions-audience.svg" loading="lazy" alt="Chart"/>
+          </div>
+
+          </div>
+          <br/> <br/> <br/>
+
+
+
+<div class="container-1">
+  <div class="paragraph-1">
+    <div className="detail">
+    <h3>TireConnect Online</h3>
+    <p >Enable customers to buy tires online <br/>without assistance in seconds</p>
+    <p className="learnMore">Learn More</p>
+
+    <p></p>
+    </div>
+  </div>
+  <div class="paragraph-1">
+  <div className="detail">
+    <h3>TireConnect in Store</h3>
+    <p>Enhance your customers experience by <br/> providing quick notes.</p>
+    <p className="learnMore">Learn More</p>
+
+    </div>
+  </div>
+  <div class="paragraph-1">
+  <div className="detail">
+    <h3>TireConnect Wheels</h3>
+    <p>Sell wheels as a standalone or as a part <br/> of wheel and tire package</p>
+    <p className="learnMore">Learn More</p>
+
+    </div>
+  </div>
+</div>
+<br/>
+<br/>
+
+
+<div class="container-1">
+  <div class="paragraph-1">
+    <div className="detail">
+    <h3>TireConnect Online</h3>
+    <p >Enable customers to buy tires online <br/>without assistance in seconds</p>
+    <p className="learnMore">Learn More</p>
+
+    <p></p>
+    </div>
+  </div>
+  <div class="paragraph-1">
+  <div className="detail">
+    <h3>TireConnect in Store</h3>
+    <p>Enhance your customers experience by <br/> providing quick notes.</p>
+    <p className="learnMore">Learn More</p>
+
+    </div>
+  </div>
+  <div class="paragraph-1">
+  <div className="detail">
+    <h3>TireConnect Wheels</h3>
+    <p>Sell wheels as a standalone or as a part <br/> of wheel and tire package</p>
+    <p className="learnMore">Learn More</p>
+
+    </div>
+  </div>
+</div>
+<br/><br/>
+
+
+<section className="section demo">
+  <div className="container">
+    <div className="demo-wrapper">
+      <div className="demo-l">
+        <h1 className="text-white" style={{fontSize:"50px", color:"white"}}>See TireConnect in <br/>action with a demo</h1>
+        <p className="paragraph-l demo-p" style={{color:"white", lineHeight:"1.5"}}>Book a demo to discover how TireConnect’s best-in-class tire shop <br/> software can become a critical component of your tire sales strategy <br/>driving revenue, market share, and customer satisfaction.</p>
+        {/* <button className="button1 " style={{color:"white", backgroundColor:"black"}}>Get a Demo</button> */}
+        <button className="button1"> Get a Demo </button>
+
+        </div> 
+        <div className="demo-r"><img src="https://uploads-ssl.webflow.com/61f95ed9bf2aea22dfc2f8b6/61f95ed9bf2aea0b4ac2f8f0_cta-img.png" loading="lazy" alt="Devices" className="demo-img"/></div></div></div>
+</section>
+<br/><br/><br/><br/>
+<div class="wrapper-flex">
+  <div class="flex-l mw-412">
+    <h1 className="benefits"><strong>Benefits for your Business</strong>
+    </h1><p className="description ">Create faster, more accurate quotes and tire information in-store and online so you can do business smarter not harder.</p></div>
+    <div class="flex-r"><img src="https://uploads-ssl.webflow.com/61f95ed9bf2aea22dfc2f8b6/61f95ed9bf2aeacd8bc2f963_man-chart.svg" loading="lazy" alt="Man Chart Illustration " class="man-chart"/></div></div>
+
+    
 
     </>
   )
